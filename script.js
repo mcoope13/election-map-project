bf471a6509d57eb0b7ebba3f9954c3fd9fa671b6
Megan <mcoope13@gmail.com>
2020-03-03 20:01:32 -0500

    updated js to count votes

--- script.js
+++ script.js
@@ -9,9 +9,47 @@ var createPolitician = function(name){
  
 };
  
-var democrat = createPolitician("Elizabeth Warrn");
+var democrat = createPolitician("Elizabeth Warren");
  
 var republican = createPolitician("Donald Trump");
+
+//Array of results
+democrat.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+
+republican.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+
+democrat.electionResults[9] = 1;
+republican.electionResults[9] = 28;
+ 
+democrat.electionResults[4] = 17;
+republican.electionResults[4] = 38;
+ 
+democrat.electionResults[43] = 11;
+republican.electionResults[43] = 27;
+ 
+console.log(democrat.electionResults);
+console.log(republican.electionResults);
+
+//method for counting votes
+politician.countTotalVotes = function ()
+{
+    this.totalVotes = 0;
+    
+    for (var i = 0; i < this.electionResults.length; i++)
+        {
+            this.totalVotes = this.totalVotes + this.electionResults[i];
+        }
+};
+
+//calling method to count votes
+democrat.countTotalVotes();
+republican.countTotalVotes;
+
+//logging to console tallied votes
+console.log(democrat.totalVotes);
+console.log(republican.totalVotes);
+
+
 
 
 /* var demPolitician = function()
