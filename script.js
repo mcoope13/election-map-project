9b0628f112392175d3836b63c0d915dc43cfe5a8
Megan <mcoope13@gmail.com>
2020-03-03 20:24:53 -0500

    adding party color values to JS

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ var createPolitician = function(name){
     politician.name = name; // set name property to the value of the name parameter
     politician.electionResults = null;
     politician.totalVotes = 0;
+    politician.partyColor = partyColor;
     
     return politician;
  
 };
  
-var democrat = createPolitician("Elizabeth Warren");
- 
-var republican = createPolitician("Donald Trump");
+//pass the party color array 
+var democrat = createPolitician("Elizabeth Warren", [132, 17, 11]);
+var republican = createPolitician("Donald Trump", [245, 141, 136]);
+
+//check to make sure the property is assigned correctly
+console.log(democrat.name + "'s party color is: " + democrat.partyColor);
+console.log(republican.name + "'s color is: " + republican.partyColor);
 
 //Array of results
 democrat.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
